83ebd599a264b0ccf390ac6377a0f59c84d55882
Passadee <passadee.ki@gmail.com>
2022-02-23 19:44:18 +0700

    day

--- appCalender/wwwroot/js/site.js
+++ appCalender/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    selectable: true,
+    headerToolbar: {
+      left: "prev,next today",
+      center: "title",
+      right: "dayGridMonth",
+    },
+    select: function (info) {
+      alert("วันที่เลือก " + info.startStr + " ถึง " + info.endStr);
+    },
+  });
+
+  calendar.render();
+});
